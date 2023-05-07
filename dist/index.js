e33edc3a743c53cf295eeba1b13930d77004dc8e
Michael <michael@boxknight.com>
2023-05-07 14:57:26 -0400

    first commit

---
+++ dist/index.js
@@ -0,0 +1,21 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+const port = process.env.PORT;
+const reqLogger = function (req, _res, next) {
+    console.info(`${req.method} request to "${req.url}" by ${req.hostname}`);
+    next();
+};
+app.use(reqLogger);
+app.get('/', (req, res) => {
+    res.send('Express + TypeScript Server');
+});
+app.listen(port, () => {
+    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
---
+++ index.ts
@@ -0,0 +1,22 @@
+import express, { Express, NextFunction, Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT;
+
+const reqLogger = function (req: Request, _res: Response, next: NextFunction) {
+  console.info(`${req.method} request to "${req.url}" by ${req.hostname}`);
+  next();
+};
+
+app.use(reqLogger)
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Express + TypeScript Server');
+});
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
